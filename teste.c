5a53a55040e82215c98421f0f374ce230047a028
brunloyola02 <larryisreal1d>
2020-02-14 08:20:06 -0300

    fiz o que o prof pediu...

--- teste.c
+++ teste.c
@@ -3,6 +3,14 @@
 
 int main(){
 
+    int num, mens;
+
+    printf("Digite a sua idade:\n");
+    scanf("%d", &num);
+
+    mens = num - 1;
+
+    printf("\nSua idade diminuída por 1 é: %d!\n", mens);
 
     return 0;
 }
